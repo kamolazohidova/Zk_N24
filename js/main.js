37a1c1b2c14cfa8efdb2d3989786c0881754cc0e
Kzohidova <kamolazohidova04@gmail.com>
2022-05-06 00:59:11 +0500

    Add the close button

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 const elDemoModalOpener = document.querySelector('.modal-opener');
 const elDemoModal = document.querySelector('#demo-modal');
 const elButtonOrange = document.getElementById("button-orange");
+const elCloseButton = document.getElementById("close-button");
 
 elDemoModalOpener.addEventListener('click', function() {
     elDemoModal.classList.add('modal-open');
 });
 
 elButtonOrange.addEventListener('click', function() {
-    elDemoModal.classList.remove('modal-open')
+    elDemoModal.classList.remove('modal-open');
+});
+
+elCloseButton.addEventListener('click', function() {
+    elDemoModal.classList.remove('modal-open');
 });
